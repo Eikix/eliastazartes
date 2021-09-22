13cd3b72f7bebfe424785b4b0060400d2880c7ef
Eikix <66871571+Eikix@users.noreply.github.com>
2021-09-22 21:51:36 +0200

    first commit added 3D

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,25 @@
-<script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
-</script>
+<script></script>
+
+<svelte:head>
+  <title>Hello man</title>
+</svelte:head>
+
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <h1 class="hi">Hello World</h1>
 </main>
 
 <style>
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    --color-primary: hsl(0, 0%, 90%);
+    --color-secondary: hsl(0, 0%, 80%);
+    
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  .hi {
+    color: var(--color-primary);
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,98 @@
-import App from './App.svelte'
+import App from './App.svelte';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(7);
+camera.position.setX(0);
+camera.position.setY(-0.5);
+
+renderer.render(scene, camera);
+
+const loader = new GLTFLoader();
+
+let mixer;
+let delta;
+let bg;
+const clock = new THREE.Clock();
+
+loader.load(
+    'cloud_station/scene.gltf',
+    function (model) {
+        console.log(model);
+        bg = model.scene;
+        bg.position.set(1, -1, -10);
+        bg.rotation.x += 0.09;
+        mixer = new THREE.AnimationMixer(model.scene);
+        console.log(mixer);
+        const animationAction = mixer.clipAction(model.animations[0]);
+        animationAction.play();
+        scene.add(model.scene);
+    },
+    undefined,
+    function (error) {
+        console.error(error);
+    }
+);
+
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Resize
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+// Animate
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    delta = clock.getDelta();
+
+    if (mixer) mixer.update(delta);
+    renderer.render(scene, camera);
+    // controls.update();
+}
+
+animate();
+
+// Svelte
 
 const app = new App({
-  target: document.getElementById('app')
-})
+    target: document.getElementById('app'),
+});
 
-export default app
+export default app;
